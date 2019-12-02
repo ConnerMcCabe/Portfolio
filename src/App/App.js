073f3d36f8305f28fcb460996070a88bbd4339b2
Admin <conner.p.mccabe@gmail.com>
2019-12-02 15:01:40 -0600

    alternate navbar

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import About from "../Component/About"
-import Contact from "../Component/Contact"
-import Proj from "../Component/Project"
+// import About from "../Component/About"
+// import Contact from "../Component/Contact"
+// import Proj from "../Component/Project"
 import './App.css';
+// import logo from "../../public/images/jumboGummyBear.png"
 
 function App() {
   return (
     <div className="App">
-      <About />
-      <Contact />
-      <Proj />
+      <div className="navigation">
+        {/* <img src={logo} /> */}
+        <div>
+          <a href="" className="item">Projects</a>
+          <a href="" className="item">About</a>
+          <a href="" className="item">Contact</a>
+        </div>
+      </div>
     </div>
   );
 }

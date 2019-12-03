03975d0d63a569fc6ed7089a2156b776c74f0692
Admin <conner.p.mccabe@gmail.com>
2019-12-03 14:29:36 -0600

    BrowserRouter

--- src/App/App.js
+++ src/App/App.js
@@ -1,22 +1,28 @@
 import React from 'react';
-// import About from "../Component/About"
-// import Contact from "../Component/Contact"
-// import Proj from "../Component/Project"
+import About from "../Component/About"
+import Contact from "../Component/Contact"
+import Proj from "../Component/Project"
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 import './App.css';
 // import logo from "../../public/images/jumboGummyBear.png"
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
+      <Route exact path="/" component={Contact} />
+      <Route path="/about" component={About} />
+      <Route path="/projects" component={Proj} />
       <div className="navigation">
         {/* <img src={logo} /> */}
         <div>
-          <a href="" className="item">Projects</a>
-          <a href="" className="item">About</a>
-          <a href="" className="item">Contact</a>
+          <Link to="/" className="item">Contact</Link>
+          <Link to="/projects" className="item">Projects</Link>
+          <Link to="/about" className="item">About</Link>
         </div>
       </div>
     </div>
+    </BrowserRouter>
   );
 }
 

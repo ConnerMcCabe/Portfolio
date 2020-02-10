94a2b25cc0741f87f428281bcf6ce362ad2aba05
Conner McCabe <conner.p.mccabe@gmail.com>
2020-02-10 15:03:57 -0600

    tweak styling before cat broke my desk

--- src/App/App.js
+++ src/App/App.js
@@ -4,23 +4,22 @@ import Contact from "../Component/Contact"
 import Proj from "../Component/Project"
 import { BrowserRouter, Route, Link } from 'react-router-dom'
 import './App.css';
-// import logo from "../../public/images/jumboGummyBear.png"
+
 
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
-      <Route exact path="/" component={Contact} />
-      <Route path="/about" component={About} />
-      <Route path="/projects" component={Proj} />
-      <div className="navigation">
-        {/* <img src={logo} /> */}
-        <div>
+    <div className="navigation">
+        <div className="navbar">
           <Link to="/" className="item">Contact</Link>
           <Link to="/projects" className="item">Projects</Link>
           <Link to="/about" className="item">About</Link>
         </div>
-      </div>
+    </div>
+      <Route exact path="/" component={Contact} />
+      <Route path="/about" component={About} />
+      <Route path="/projects" component={Proj} />
     </div>
     </BrowserRouter>
   );

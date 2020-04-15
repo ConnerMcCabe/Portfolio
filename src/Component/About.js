fabf5950394495404d28886f4f40163d57405c4e
Conner McCabe <conner.p.mccabe@gmail.com>
2020-04-15 13:29:49 -0500

    adding about section git button

--- src/Component/About.js
+++ src/Component/About.js
@@ -7,10 +7,12 @@ function About(props) {
       <br></br>
       <p contentEditable="true" className="bio">
         I am an adaptable software developer that is into making creative problems to modern solutions
-         using the many different perspectives I've picked up along the way. I like to see the big picture 
-         and am all about breaking down problems into manageable solutions; give me a big enough whiteboard 
-         and I can conquer the world
+        using the many different perspectives I've picked up along the way. I like to see the big picture 
+        and am all about breaking down problems into manageable solutions; give me a big enough whiteboard 
+        and I can conquer the world
       </p>
+      <a className="buttons" href="https://github.com/ConnerMcCabe" target="_blank" rel="noopener noreferrer">click here</a>
+      
     </div>
   )
 }

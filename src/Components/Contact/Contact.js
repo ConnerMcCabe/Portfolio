d2177606e29c2a009f460999d590c8bf24f96fb1
Conner McCabe <conner.p.mccabe@gmail.com>
2020-04-22 10:18:29 -0500

    styling work

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -5,14 +5,14 @@ import React from "react"
 function Contact(props) {
   return (
     <div className="contact">
-        <h1>About me</h1>
+        {/* <h1>About me</h1>
         <p contentEditable="true" className="bio">
           I am an adaptable software developer that is into making creative problems to modern solutions
           using the many different perspectives I've picked up along the way. I like to see the big picture 
           and am all about breaking down problems into manageable solutions; give me a big enough whiteboard 
           and I can conquer the world
         </p>
-      <a className="buttons" href="https://github.com/ConnerMcCabe" target="_blank" rel="noopener noreferrer">click here</a>
+      <a className="buttons" href="https://github.com/ConnerMcCabe" target="_blank" rel="noopener noreferrer">click here</a> */}
       <div className='container'>
       <h1>Contact Me</h1>
       <p>Thanks for taking the time to reach out. How can I help you today?</p>

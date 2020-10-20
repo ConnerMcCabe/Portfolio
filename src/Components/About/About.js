4b0ceaa0de4a0a0133698ef12edc66e0db36081c
Conner McCabe <conner.p.mccabe@gmail.com>
2020-10-20 12:32:05 -0500

    tweaks to about page

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -6,14 +6,14 @@ function About() {
       <div className='container'>
         <h1>About Me</h1>
           <div>
-            <p /*contentEditable="true"*/ className="bio">
+            <p className="bio">
               I'm an adaptable software developer that is into making creative problems to modern solutions
               using the many different perspectives I've picked up along the way. I like to see the big picture 
               and am all about breaking down problems into manageable solutions; give me a big enough whiteboard 
               and I can conquer the world
             </p>
             
-          <a className="buttons" href="https://github.com/ConnerMcCabe" target="_blank" rel="noopener noreferrer"></a> 
+          <a className="buttons" href="https://github.com/ConnerMcCabe" target="_blank" rel="noopener noreferrer">button</a> 
             <h1>blog info</h1>
             <h1>timeline</h1>
           </div>

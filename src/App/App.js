e732576f0dd6f7273f9ab264cdf80ccdde354ea5
Conner McCabe <conner.p.mccabe@gmail.com>
2021-03-08 14:44:39 -0600

    replacing buttons with relevant icons

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import './App.css';
 
 import Navbar from '../Components/Navbar/Navbar.js'
@@ -8,15 +9,16 @@ import Contact from "../Components/Contact/Contact.js"
 import Portfolio from "../Components/Portfolio/Portfolio.js"
 
 
+
 function App() {
   return (
     <BrowserRouter>
-    <div className="App">
-    <Navbar />
-      <Route exact path="/contact" component={Contact} />
-      <Route path="/about" component={About} />
-      <Route path="/projects" component={Portfolio} />
-    </div>
+      <div className="App">
+        <Navbar />
+        <Route exact path="/contact" component={Contact} />
+        <Route path="/about" component={About} />
+        <Route path="/projects" component={Portfolio} />
+      </div>
     </BrowserRouter>
   );
 }
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
 import './Navbar.css';
 
 function Navbar() {
@@ -11,8 +13,9 @@ function Navbar() {
                 <Link to="/about" className="navItem">About</Link>
             </div>
             <div className="nav-Connections">
-                <a className="buttons" href="https://github.com/ConnerMcCabe" target="_blank" rel="noopener noreferrer">GitHub</a>
-                <a className="buttons" href="https://www.linkedin.com/in/conner-p-mccabe/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+                
+                <a className="navLink" href="https://github.com/ConnerMcCabe" target="_blank" rel="noopener noreferrer"><GitHubIcon /></a>
+                <a className="navLink" href="https://www.linkedin.com/in/conner-p-mccabe/" target="_blank" rel="noopener noreferrer"><LinkedInIcon /></a>
             </div>
         </div>
     )
